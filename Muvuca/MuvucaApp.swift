e2181601d00eb9b04d6db6c071e44f0ceb21123d
Erick Almeida <erickingles@gmail.com>
2021-02-08 11:32:30 -0300

    Realiza primeiro commit

---
+++ Muvuca/MuvucaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuvucaApp.swift
+//  Muvuca
+//
+//  Created by Erick Almeida on 08/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MuvucaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
