ba11a6ea05376231f1fa6aa030e4bd1ce4910372
Alanis <alanislimasantaclara00@gmail.com>
2021-02-10 08:54:06 -0300

    Adiciona Firebase e modelo

---
+++ Muvuca/FirebaseHandler.swift
@@ -0,0 +1,20 @@
+//
+//  FirebaseHandler.swift
+//  Muvuca
+//
+//  Created by Alanis Lima Santa Clara on 10/02/21.
+//
+
+import SwiftUI
+
+struct FirebaseHandler: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirebaseHandler_Previews: PreviewProvider {
+    static var previews: some View {
+        FirebaseHandler()
+    }
+}
